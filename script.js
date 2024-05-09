4f9a97872a30faba65da8b2a9338686d4f23ed81
DevoMore <devorah_alessandra@hotmail.com>
2024-05-09 17:58:57 +0200

    esperimento xd

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const ciao = document.getElementById('para');
+const frutta = 'Fragole';
+switch (frutta) {
+  case 'Arancie':
+    console.log('Le arancie costano 2 euro.');
+    break;
+  case 'Fragole':
+  case 'Mele':
+    console.log('Fragole e mele costano 3 euro.');
+    break;
+  default:
+    console.log(`Non abbiamo più ${frutta}.`);
+}
